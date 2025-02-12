0f373e0de0ae18e0410380e97faf3b58c622199d
makusets <68693442+makusets@users.noreply.github.com>
2025-02-12 13:15:33 +1100

    Update climate.py

--- components/tcc_link/climate.py
+++ components/tcc_link/climate.py
@@ -82,7 +82,7 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
             }
         ),
         cv.Optional(CONF_ESP_SENSOR_TEMP): sensor.sensor_schema(
-            unit_of_measurement=UNIT_CELSIUS,
+            # unit_of_measurement=UNIT_CELSIUS,
             accuracy_decimals=1,
             device_class=DEVICE_CLASS_TEMPERATURE,
             state_class=STATE_CLASS_MEASUREMENT,
