8171d8e5566ee8ccebc704bb3910d1bdb2d557d3
makusets <68693442+makusets@users.noreply.github.com>
2025-02-12 12:29:37 +1100

    Update climate.py

--- components/tcc_link/climate.py
+++ components/tcc_link/climate.py
@@ -80,7 +80,7 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
             }
         ),
     }
-).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA).extend(sensor.sensor_schema())
+).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA).extend(sensor.SENSOR_SCHEMA) #added last extend
 
 def validate_uart(config):
     uart.final_validate_device_schema(
