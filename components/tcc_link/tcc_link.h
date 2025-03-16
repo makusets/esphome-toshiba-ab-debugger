df9fadbcf3f573a47ee39694dd07a31387dde58e
makusets <68693442+makusets@users.noreply.github.com>
2025-03-16 20:27:00 +1100

    h

--- components/tcc_link/tcc_link.h
+++ components/tcc_link/tcc_link.h
@@ -5,7 +5,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/uart/uart.h"
-#include "esphome/components/bme280_i2c/bme280_i2c.h"
+//#include "esphome/components/bme280_i2c/bme280_i2c.h"
 #include <bitset>
 #include <queue>
 #include <Wire.h>
