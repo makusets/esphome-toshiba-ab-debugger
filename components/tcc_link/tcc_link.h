44a13e8ef193ea664163a7149aa6f946a29486f7
makusets <68693442+makusets@users.noreply.github.com>
2025-04-18 12:14:18 +1000

    u

--- components/tcc_link/tcc_link.h
+++ components/tcc_link/tcc_link.h
@@ -6,6 +6,8 @@
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/uart/uart.h"
 // #include "esphome/components/bme280_i2c/bme280_i2c.h"  esphome cannot find it?
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/bme280/bme280.h"
 #include <bitset>
 #include <queue>
 #include <Wire.h>
