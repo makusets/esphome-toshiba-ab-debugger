fa93ee0eafbcce74672947c8230ac6ed96e1ec91
makusets <68693442+makusets@users.noreply.github.com>
2025-05-07 23:06:30 +1000

    u

--- components/toshiba_ab/toshiba_ab.cpp
+++ components/toshiba_ab/toshiba_ab.cpp
@@ -340,12 +340,10 @@ void ToshibaAbClimate::sync_from_received_state() {
     changes++;
   }
 
-  if (tcc_state.target_temp > 15 && tcc_state.target_temp <30 && target_temperature != tcc_state.target_temp) {
+  if (target_temperature != tcc_state.target_temp) {
     target_temperature = tcc_state.target_temp;
     changes++;
-  }else {
-    ESP_LOGW(TAG, "Invalid target temperature: %f", tcc_state.target_temp);}
-  
+  }
 
   if (current_temperature != tcc_state.room_temp) {
     current_temperature = tcc_state.room_temp;
