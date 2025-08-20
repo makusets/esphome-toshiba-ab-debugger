eb9a821e42237ae281ae0d201a7fa12522041a04
makusets <68693442+makusets@users.noreply.github.com>
2025-08-21 08:15:07 +1000

    u

--- components/toshiba_ab/__init__.py
+++ components/toshiba_ab/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor
+from esphome.components import uart
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
